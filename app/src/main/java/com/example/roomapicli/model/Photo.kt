c95381bf50569c0aba8ad30cea3f8ba57a3e9200
jnefta91 <e.josem91@gmail.com>
2019-12-22 17:58:21 -0600

    add model for set json

--- app/src/main/java/com/example/roomapicli/model/Photo.kt
+++ app/src/main/java/com/example/roomapicli/model/Photo.kt
@@ -1,4 +1,10 @@
 package com.example.roomapicli.model
 
-class Photo {
+data class Photo (
+    var albumId : Int,
+    var id : Int,
+    var title : String,
+    var url : String,
+    var thumbnailUrl: String)
+{
 }
