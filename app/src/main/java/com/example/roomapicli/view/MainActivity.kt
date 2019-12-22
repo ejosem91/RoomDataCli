878f9ac6c9c28748bab8c5dfa0480c8dd5d6398e
jnefta91 <e.josem91@gmail.com>
2019-12-22 12:25:20 -0600

    add package

--- app/src/main/java/com/example/roomapicli/view/MainActivity.kt
+++ app/src/main/java/com/example/roomapicli/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.roomapicli
+package com.example.roomapicli.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.roomapicli.R
 
 class MainActivity : AppCompatActivity() {
 
