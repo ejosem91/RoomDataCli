b696e006c1b40585d4f9704ee95bc20c7f45da2e
jnefta91 <e.josem91@gmail.com>
2019-12-23 11:05:47 -0600

    order code

--- app/src/main/java/com/example/roomapicli/api/Api.kt
+++ app/src/main/java/com/example/roomapicli/api/Api.kt
@@ -1,2 +1,19 @@
 package com.example.roomapicli.api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Api{
+    private const val url = "https://jsonplaceholder.typicode.com/"
+    /**
+     * @return Retrofit instance for getting API data
+     **/
+    fun retrofit(): ApiInterface {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(url)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit.create(ApiInterface::class.java)
+    }
+
+}
