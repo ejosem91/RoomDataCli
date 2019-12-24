fc41e6bd8279a1835b1e5871b95661ad69c76a4b
jnefta91 <e.josem91@gmail.com>
2019-12-24 10:37:36 -0600

    add adapter for recycler view

---
+++ app/src/main/java/com/example/roomapicli/adapter/PhotoAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.roomapicli.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.roomapicli.model.Photo
+import com.example.roomapicli.R
+import com.example.roomapicli.databinding.ItemPhotoBinding
+import com.example.roomapicli.viewmodel.ItemsPhotoViewModel
+
+
+class PhotoAdapter(listPhotos: List<Photo>, val liveData: MutableLiveData<Photo>):RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
+    private val listPhotoData = listPhotos
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_photo, parent, false))
+    }
+
+    override fun getItemCount() = listPhotoData.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(listPhotoData[position])
+
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding: ItemPhotoBinding? = DataBindingUtil.bind(view)
+
+        fun bind(item : Photo) {
+
+            //itemView.setOnClickListener{liveData.value = item}
+
+            binding?.itemsPhotoViewModel = ItemsPhotoViewModel(item ,liveData)
+
+            binding?.imagePhoto?.context?.let {
+                Glide.with(it).load(item.url).into(binding.imagePhoto)
+            }
+
+        }
+    }
+}
