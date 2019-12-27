0e6b2a87750f43abf3488f3ddd60e893abe053bb
jnefta91 <e.josem91@gmail.com>
2019-12-27 08:16:03 -0600

    add observe from view model

--- app/src/main/java/com/example/roomapicli/view/MainActivity.kt
+++ app/src/main/java/com/example/roomapicli/view/MainActivity.kt
@@ -1,52 +1,69 @@
 package com.example.roomapicli.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roomapicli.R
-import com.example.roomapicli.api.Api
+import com.example.roomapicli.adapter.PhotoAdapter
+import com.example.roomapicli.database.PhotoDao
+import com.example.roomapicli.database.PhotoDataBase
 import com.example.roomapicli.databinding.ActivityMainBinding
 import com.example.roomapicli.model.Photo
-import com.example.roomapicli.viewmodel.PhotoModelView
-import retrofit2.Call
-import retrofit2.Response
+import com.example.roomapicli.repository.RepositoryGetPhoto
+import com.example.roomapicli.viewmodel.FactoryPhotoViewModel
+import com.example.roomapicli.viewmodel.PhotoViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: PhotoModelView
+    private lateinit var viewModel: PhotoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModel = ViewModelProviders.of(this).get(PhotoModelView::class.java)
 
+        val db :PhotoDataBase?  = PhotoDataBase.getInstance(context =  applicationContext)
+        //val photoDao : PhotoDao? = db?.photoDao()
+
+        val viewModelFactory = FactoryPhotoViewModel(RepositoryGetPhoto(db))
+
+        viewModel = ViewModelProviders.of(this,viewModelFactory).get(PhotoViewModel::class.java)
         binding.viewModel = viewModel
-        showData()
+        show()
+        viewModel.liveData.observe( this , clickObserver() )
     }
 
-    private fun showData() {
-        binding.viewModel?.getListPhoto()?.observe(this, Observer {
-            Log.v("MainActivity", " ${it.size}")
+    private fun show() {
+        viewModel?.photoListData?.observe(this, Observer {
+            val adapterPhotos = PhotoAdapter(it,viewModel.liveData)
+            binding.photoRecycler.layoutManager = LinearLayoutManager(this)
+            binding.photoRecycler.adapter = adapterPhotos
         })
 
     }
 
+    private  fun clickObserver() = Observer<Photo> {
+        val detailClassIntent = Intent(
+            this,
+            DetailActivity::class.java
+        )
+        val bundle = Bundle()
+        bundle.putParcelable(BUNDLE_KEY, it)
+        detailClassIntent.putExtras(bundle)
+        ContextCompat.startActivity(this, detailClassIntent, null)
 
-   /* private fun getData() {
-        val call: Call<List<Photo>> = Api.retrofit().getPhotos()
-        call.enqueue(object : retrofit2.Callback<List<Photo>> {
-            override fun onResponse(call: Call<List<Photo>>?, response: Response<List<Photo>>?) {
-                Log.v("MainActivity", "${response?.body()?.size} ")
+        it.let {
+            Log.v("LogWF", "Click")
+        }
+    }
 
-            }
-
-            override fun onFailure(call: Call<List<Photo>>?, t: Throwable?) {
-            }
-        })
-    }*/
-
+    companion object{
+        const val BUNDLE_KEY = "details"
+    }
 }
