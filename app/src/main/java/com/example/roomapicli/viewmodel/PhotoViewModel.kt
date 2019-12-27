4ccf14d3f49f1e1afbccd584bbc74baa2368727d
jnefta91 <e.josem91@gmail.com>
2019-12-27 08:11:43 -0600

    add launch for coroutine

--- app/src/main/java/com/example/roomapicli/viewmodel/PhotoViewModel.kt
+++ app/src/main/java/com/example/roomapicli/viewmodel/PhotoViewModel.kt
@@ -1,16 +1,41 @@
 package com.example.roomapicli.viewmodel
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+
+import android.util.Log
+import androidx.lifecycle.*
 import com.example.roomapicli.model.Photo
 import com.example.roomapicli.repository.RepositoryGetPhoto
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class PhotoModelView: ViewModel() {
+class PhotoViewModel(private val repositoryGetPhoto: RepositoryGetPhoto) : ViewModel() {
 
-    var dataList : RepositoryGetPhoto = RepositoryGetPhoto()
 
-    fun getListPhoto(): MutableLiveData<List<Photo>> {
-        return dataList.getPhotoData()
+    init{
+        getPhotoList()
     }
 
+//    fun getListPhoto(): MutableLiveData<List<Photo>> {
+//        return repositoryGetPhoto.getPhotoData()
+//    }
+
+    var liveData: MutableLiveData<Photo> = MutableLiveData()
+    var photoListLiveDataSource: MutableLiveData<List<Photo>> = MutableLiveData()
+
+    var photoListData = MediatorLiveData<List<Photo>>()
+
+    private fun getPhotoList() = viewModelScope.launch(Dispatchers.Main) {
+        withContext(Dispatchers.IO) {
+            photoListLiveDataSource = repositoryGetPhoto.getPhotoDataAPI()
+        }
+        photoListData.value = photoListLiveDataSource.value
+    }
+}
+
+class FactoryPhotoViewModel(private val photoDao: RepositoryGetPhoto) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return PhotoViewModel(photoDao) as T
+    }
 }
