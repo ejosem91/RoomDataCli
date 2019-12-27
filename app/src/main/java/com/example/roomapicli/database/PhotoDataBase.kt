a65ed6937379a6194e34d5eb127193a2bdcf9225
jnefta91 <e.josem91@gmail.com>
2019-12-27 08:26:53 -0600

    update class Photo on reference

---
+++ app/src/main/java/com/example/roomapicli/database/PhotoDataBase.kt
@@ -0,0 +1,28 @@
+package com.example.roomapicli.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.roomapicli.model.Photo
+
+
+@Database(entities = [Photo::class], version = 1, exportSchema = false)
+abstract class PhotoDataBase : RoomDatabase() {
+    abstract  fun photoDao() : PhotoDao
+    companion object {
+        private var INSTANCE: PhotoDataBase? = null
+        fun getInstance(context: Context): PhotoDataBase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext
+                        , PhotoDataBase::class.java, "photo_database"
+                    ).build()
+                }
+                return instance
+            }
+        }
+    }
+}
