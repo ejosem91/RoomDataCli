73e89997ee00e5d3f3a48d3ee261628c54fcf4a3
jnefta91 <e.josem91@gmail.com>
2019-12-27 08:24:45 -0600

    update instance retrofit for deferred support

--- app/src/main/java/com/example/roomapicli/api/Api.kt
+++ app/src/main/java/com/example/roomapicli/api/Api.kt
@@ -1,5 +1,6 @@
 package com.example.roomapicli.api
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -12,6 +13,7 @@ object Api{
         val retrofit = Retrofit.Builder()
             .baseUrl(url)
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
             .build()
         return retrofit.create(ApiInterface::class.java)
     }
