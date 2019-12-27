053ae17c6ea4df2a72fa640333130166cf13b749
jnefta91 <e.josem91@gmail.com>
2019-12-27 08:18:46 -0600

    update method  from call to Deferred

--- app/src/main/java/com/example/roomapicli/api/ApiInterface.kt
+++ app/src/main/java/com/example/roomapicli/api/ApiInterface.kt
@@ -1,11 +1,11 @@
 package com.example.roomapicli.api
 
 import com.example.roomapicli.model.Photo
-import retrofit2.Call
+import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 
 interface ApiInterface {
 
     @GET("photos")
-    fun getPhotos(): Call<List<Photo>>
+    fun getPhotosAsync(): Deferred<List<Photo>>
 }
