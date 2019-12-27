89e1fa1cfb80cd47dea450477eea8a373ec6b8f1
jnefta91 <e.josem91@gmail.com>
2019-12-27 08:12:53 -0600

    add Room notation

--- app/src/main/java/com/example/roomapicli/model/Photo.kt
+++ app/src/main/java/com/example/roomapicli/model/Photo.kt
@@ -1,8 +1,46 @@
 package com.example.roomapicli.model
 
-data class Photo (
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+@Entity(tableName = "photo")
+ class Photo (
+    @PrimaryKey
+    var id : Int?,
     var albumId : Int,
-    var id : Int,
-    var title : String,
-    var url : String,
-    var thumbnailUrl: String)
+    var title : String?,
+    var url : String?,
+    var thumbnailUrl : String?
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeValue(id)
+        parcel.writeInt(albumId)
+        parcel.writeString(title)
+        parcel.writeString(url)
+        parcel.writeString(thumbnailUrl)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Photo> {
+        override fun createFromParcel(parcel: Parcel): Photo {
+            return Photo(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Photo?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
