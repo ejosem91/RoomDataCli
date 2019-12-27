fe0bb2fab983a74b1b3ecf729955f736cb9e1068
jnefta91 <e.josem91@gmail.com>
2019-12-27 08:13:39 -0600

    add activity for show image

---
+++ app/src/main/java/com/example/roomapicli/view/DetailActivity.kt
@@ -0,0 +1,44 @@
+package com.example.roomapicli.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
+import com.example.roomapicli.R
+import com.example.roomapicli.databinding.ActivityDetailBinding
+import com.example.roomapicli.model.Photo
+import com.example.roomapicli.viewmodel.DetailViewModel
+
+class DetailActivity : AppCompatActivity() {
+
+
+    private  lateinit var  binding : ActivityDetailBinding
+    private lateinit var detailViewModel: DetailViewModel
+    private var photos: Photo? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        val bundleReceived = intent.extras
+        bundleReceived?.getParcelable<Photo>(BUNDLE_KEY).let { photos = it }
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_detail
+        )
+        detailViewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
+        binding.detailViewModel = detailViewModel
+        setImageUrl(binding.detailedImage, photos)
+    }
+    private fun setImageUrl(view: ImageView, detailModel: Photo?) {
+        Glide.with(view.context).load(detailModel?.url)
+            .into(view)
+    }
+
+    companion object{
+        const val BUNDLE_KEY = "details"
+    }
+}
