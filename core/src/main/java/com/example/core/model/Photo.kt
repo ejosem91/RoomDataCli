7a374ddd51309f42dc845c7e666f0a77a2aaf519
jnefta91 <e.josem91@gmail.com>
2020-01-06 16:28:08 -0600

    delete parcelables on my model

--- core/src/main/java/com/example/core/model/Photo.kt
+++ core/src/main/java/com/example/core/model/Photo.kt
@@ -1,42 +1,9 @@
 package com.example.core.model
 
-import android.os.Parcel
-import android.os.Parcelable
-
 data class Photo(
     var id: Int?,
     var albumId: Int,
     var title: String?,
     var url: String?,
     var thumbnailUrl: String?
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readInt(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeValue(id)
-        parcel.writeInt(albumId)
-        parcel.writeString(title)
-        parcel.writeString(url)
-        parcel.writeString(thumbnailUrl)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Photo> {
-        override fun createFromParcel(parcel: Parcel): Photo {
-            return Photo(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Photo?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+)
