6e3f232256e727d367039ab625d1d75abb95b91a
jnefta91 <e.josem91@gmail.com>
2020-01-06 16:08:40 -0600

    add dependencies

--- core/src/main/java/com/example/core/repository/PhotoDataRepository.kt
+++ core/src/main/java/com/example/core/repository/PhotoDataRepository.kt
@@ -3,7 +3,6 @@ package com.example.core.repository
 import androidx.lifecycle.LiveData
 import com.example.core.model.Photo
 
-
 interface PhotoDataRepository {
-    suspend fun getDataPhoto(): LiveData<List<Photo>>
+    suspend fun getDataPhoto(): LiveData<MutableList<Photo>>
 }
