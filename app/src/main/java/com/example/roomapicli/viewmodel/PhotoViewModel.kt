b04ce9310b73d064e6fb7cf24a6a313c9c731984
jnefta91 <e.josem91@gmail.com>
2020-01-06 14:48:01 -0600

    add use case for getting data

--- app/src/main/java/com/example/roomapicli/viewmodel/PhotoViewModel.kt
+++ app/src/main/java/com/example/roomapicli/viewmodel/PhotoViewModel.kt
@@ -2,34 +2,34 @@ package com.example.roomapicli.viewmodel
 
 
 import androidx.lifecycle.*
-import com.example.roomapicli.model.Photo
-import com.example.roomapicli.repository.RepositoryGetPhoto
+import com.example.core.model.Photo
+import com.example.core.usecase.PhotoUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class PhotoViewModel(private val repositoryGetPhoto: RepositoryGetPhoto) : ViewModel() {
+class PhotoViewModel(private val userCase: PhotoUseCase) : ViewModel() {
 
     init {
         getPhotoList()
     }
 
     var liveDataEvent: MutableLiveData<Photo> = MutableLiveData()
-    private var photoListLiveDataSource: MutableLiveData<List<Photo>> = MutableLiveData()
+    private var photoListLiveDataSource: LiveData<MutableList<Photo>> = MutableLiveData()
 
     var photoListData = MediatorLiveData<List<Photo>>()
 
     private fun getPhotoList() = viewModelScope.launch(Dispatchers.Main) {
         withContext(Dispatchers.IO) {
-            photoListLiveDataSource = repositoryGetPhoto.getPhotoDataAPI()
+            photoListLiveDataSource = userCase.getPhotoData()
         }
         photoListData.value = photoListLiveDataSource.value
     }
 }
 
-class FactoryPhotoViewModel(private val photoDao: RepositoryGetPhoto) : ViewModelProvider.Factory {
+class FactoryPhotoViewModel(private val useCasePhoto: PhotoUseCase) : ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return PhotoViewModel(photoDao) as T
+        return PhotoViewModel(useCasePhoto) as T
     }
 }
