0f2e015aff69ab1339cc1b7575ecdbe50d0d385b
jnefta91 <e.josem91@gmail.com>
2020-01-06 14:13:52 -0600

    add class for getting data

--- data/src/main/java/com/example/data/repository/PhotoDataRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/PhotoDataRepositoryImpl.kt
@@ -6,22 +6,25 @@ import com.example.core.repository.PhotoDataRepository
 import com.example.data.api.Api
 import com.example.data.database.PhotoDataBase
 
-class PhotoDataRepositoryImpl( api: Api , private val dataBase : PhotoDataBase):PhotoDataRepository {
-
+class PhotoDataRepositoryImpl(api: Api, private val dataBase: PhotoDataBase) : PhotoDataRepository {
 
     private val client = api.retrofit()
 
-    override suspend fun getDataPhoto(): MutableLiveData<List<Photo>> {
-        val liveData = MutableLiveData<List<Photo>>()
+    override suspend fun getDataPhoto(): MutableLiveData<MutableList<Photo>> {
+        val liveData = MutableLiveData<MutableList<Photo>>()
+
         val listPhoto = client.getPhotosAsync().await()
 
-        val getList = listPhoto.subList(0,24)
-
-
+        val getList = listPhoto.subList(0, 24)
         getList.forEach {
-            dataBase?.photoDao()?.insert(it)
+            dataBase.photoDao().insert(it.mapToRoomModel())
         }
+        val listItems: MutableList<Photo> = mutableListOf()
+        dataBase.photoDao().getAll().forEach {
+            listItems.add(it.mapToDomainModel())
+        }
+        liveData.postValue(listItems)
 
-        return  liveData.postValue(dataBase?.photoDao()?.getAll())
+        return liveData
     }
 }
