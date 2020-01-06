9579af75c86136ab270a03409e7cd24771238cb7
jnefta91 <e.josem91@gmail.com>
2020-01-06 14:20:52 -0600

    add notation

--- data/src/main/java/com/example/data/model/PhotoEntity.kt
+++ data/src/main/java/com/example/data/model/PhotoEntity.kt
@@ -4,16 +4,22 @@ import android.os.Parcel
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.core.model.Photo
+import com.example.data.domain.mapperDomain
 
 @Entity(tableName = "photo")
-class Photo (
+class PhotoEntity(
     @PrimaryKey
-    var id : Int?,
-    var albumId : Int,
-    var title : String?,
-    var url : String?,
-    var thumbnailUrl : String?
-): Parcelable{
+    var id: Int?,
+    var albumId: Int,
+    var title: String?,
+    var url: String?,
+    var thumbnailUrl: String?
+) : Parcelable, mapperDomain<Photo> {
+    override fun mapToDomainModel() =
+        Photo(id = id, albumId = albumId, title =  title, url = url, thumbnailUrl = thumbnailUrl)
+
+
     constructor(parcel: Parcel) : this(
         parcel.readValue(Int::class.java.classLoader) as? Int,
         parcel.readInt(),
@@ -21,6 +27,7 @@ class Photo (
         parcel.readString(),
         parcel.readString()
     )
+
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeValue(id)
         parcel.writeInt(albumId)
@@ -33,12 +40,12 @@ class Photo (
         return 0
     }
 
-    companion object CREATOR : Parcelable.Creator<Photo> {
-        override fun createFromParcel(parcel: Parcel): Photo {
-            return Photo(parcel)
+    companion object CREATOR : Parcelable.Creator<PhotoEntity> {
+        override fun createFromParcel(parcel: Parcel): PhotoEntity {
+            return PhotoEntity(parcel)
         }
 
-        override fun newArray(size: Int): Array<Photo?> {
+        override fun newArray(size: Int): Array<PhotoEntity?> {
             return arrayOfNulls(size)
         }
     }
