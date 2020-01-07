e0170ba724ea5927bf23d7d839ec5b993a01a765
jnefta91 <e.josem91@gmail.com>
2020-01-06 18:00:36 -0600

    add event to class   ass parameter on constructor

--- app/src/main/java/com/example/roomapicli/viewmodel/ItemsPhotoViewModel.kt
+++ app/src/main/java/com/example/roomapicli/viewmodel/ItemsPhotoViewModel.kt
@@ -3,11 +3,12 @@ package com.example.roomapicli.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.core.model.Photo
+import com.example.roomapicli.util.Event
 
-class ItemsPhotoViewModel(var photo : Photo, private  var liveData : MutableLiveData<Photo>):ViewModel(){
+class ItemsPhotoViewModel(var photo : Photo, private  var photoEvent : MutableLiveData<Event<Photo>>):ViewModel(){
 
     fun onClick(){
-        liveData.value = photo
+        photoEvent.value = Event(photo)
     }
 
 }
