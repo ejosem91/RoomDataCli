6b08d4c5777f680ea7439ab5f48414265bf28b04
jnefta91 <e.josem91@gmail.com>
2020-01-13 17:43:04 -0600

    add list on insert method for sending  list

--- data/src/main/java/com/example/data/repository/PhotoDataRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/PhotoDataRepositoryImpl.kt
@@ -5,19 +5,23 @@ import com.example.core.model.Photo
 import com.example.core.repository.PhotoDataRepository
 import com.example.data.api.ApiInterface
 import com.example.data.database.PhotoDao
+import com.example.data.model.PhotoEntity
 
 class PhotoDataRepositoryImpl(private val api: ApiInterface, private val dataBaseDao: PhotoDao) : PhotoDataRepository {
 
     override suspend fun getDataPhoto(): MutableLiveData<MutableList<Photo>> {
         val liveData = MutableLiveData<MutableList<Photo>>()
-
         val listPhoto = api.getPhotosAsync().await()
-
         val getList = listPhoto.subList(0, 24)
+        val listItem :MutableList<PhotoEntity> = mutableListOf()
+
         getList.forEach {
-            dataBaseDao.insert(it.mapToRoomModel())
+            listItem.add(it.mapToRoomModel())
         }
+        dataBaseDao.insert(listItem)
+
         val listItems: MutableList<Photo> = mutableListOf()
+
         dataBaseDao.getAll().forEach {
             listItems.add(it.mapToDomainModel())
         }
