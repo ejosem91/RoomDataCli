893f114e91ca43e3279a9737057745cf064b481f
jnefta91 <e.josem91@gmail.com>
2020-01-13 11:08:06 -0600

    update  parameters on constructor class

--- data/src/main/java/com/example/data/repository/PhotoDataRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repository/PhotoDataRepositoryImpl.kt
@@ -3,24 +3,22 @@ package com.example.data.repository
 import androidx.lifecycle.MutableLiveData
 import com.example.core.model.Photo
 import com.example.core.repository.PhotoDataRepository
-import com.example.data.api.Api
-import com.example.data.database.PhotoDataBase
+import com.example.data.api.ApiInterface
+import com.example.data.database.PhotoDao
 
-class PhotoDataRepositoryImpl(api: Api, private val dataBase: PhotoDataBase) : PhotoDataRepository {
-
-    private val client = api.retrofit()
+class PhotoDataRepositoryImpl(private val api: ApiInterface, private val dataBaseDao: PhotoDao) : PhotoDataRepository {
 
     override suspend fun getDataPhoto(): MutableLiveData<MutableList<Photo>> {
         val liveData = MutableLiveData<MutableList<Photo>>()
 
-        val listPhoto = client.getPhotosAsync().await()
+        val listPhoto = api.getPhotosAsync().await()
 
         val getList = listPhoto.subList(0, 24)
         getList.forEach {
-            dataBase.photoDao().insert(it.mapToRoomModel())
+            dataBaseDao.insert(it.mapToRoomModel())
         }
         val listItems: MutableList<Photo> = mutableListOf()
-        dataBase.photoDao().getAll().forEach {
+        dataBaseDao.getAll().forEach {
             listItems.add(it.mapToDomainModel())
         }
         liveData.postValue(listItems)
